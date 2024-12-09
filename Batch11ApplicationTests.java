238e9b4fa841a45c0d99d7b5ca323d04a5c7477d
saikumar551 <saikumar02714@gmail.com>
2024-12-09 13:54:53 +0530

    Update Batch11ApplicationTests.java

---
+++ Batch11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tejait.batch11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Batch11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
